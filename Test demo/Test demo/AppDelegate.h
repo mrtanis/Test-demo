d25a8db74a25fe56724d8ae25f966a07abbaa939
mrtanis <mrtanis@mrtanisdemacbook-air.local>
2017-04-23 22:33:05 +0000

    push本地项目到GitHub

---
+++ Test demo/Test demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test demo
+//
+//  Created by mrtanis on 2017/4/23.
+//  Copyright © 2017年 mrtanis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
